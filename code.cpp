19d192668cf27ec2d1152302dc85140ec9950537
zichixu <zichixu@outlook.com>
2024-08-06 21:59:14 -0500

    Added everything since last time

--- code.cpp
+++ code.cpp
@@ -3,6 +3,8 @@
 #include <random>
 #include <cstring>
 #include <iostream>
+#include <SDL.h>
+
 
 
 using namespace std;
@@ -42,11 +44,260 @@ u_int8_t fontset[FONTSET_SIZE] = {
 
 const unsigned int VIDEO_WIDTH = 64;
 const unsigned int VIDEO_HEIGHT = 32;
+const unsigned int KEY_COUNT = 16;
+
+class Platform
+{
+public:
+	Platform(char const* title, int windowWidth, int windowHeight, int textureWidth, int textureHeight)
+	{
+		SDL_Init(SDL_INIT_VIDEO);
+
+		window = SDL_CreateWindow(title, 0, 0, windowWidth, windowHeight, SDL_WINDOW_SHOWN);
+
+		renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+		texture = SDL_CreateTexture(
+			renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, textureWidth, textureHeight);
+	}
+
+	~Platform()
+	{
+		SDL_DestroyTexture(texture);
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+	}
+
+	void Update(void const* buffer, int pitch)
+	{
+		SDL_UpdateTexture(texture, nullptr, buffer, pitch);
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+		SDL_RenderPresent(renderer);
+	}
+
+	bool ProcessInput(uint8_t* keys)
+	{
+		bool quit = false;
+
+		SDL_Event event;
+
+		while (SDL_PollEvent(&event))
+		{
+			switch (event.type)
+			{
+				case SDL_QUIT:
+				{
+					quit = true;
+				} break;
+
+				case SDL_KEYDOWN:
+				{
+					switch (event.key.keysym.sym)
+					{
+						case SDLK_ESCAPE:
+						{
+							quit = true;
+						} break;
+
+						case SDLK_x:
+						{
+							keys[0] = 1;
+						} break;
+
+						case SDLK_1:
+						{
+							keys[1] = 1;
+						} break;
+
+						case SDLK_2:
+						{
+							keys[2] = 1;
+						} break;
+
+						case SDLK_3:
+						{
+							keys[3] = 1;
+						} break;
+
+						case SDLK_q:
+						{
+							keys[4] = 1;
+						} break;
+
+						case SDLK_w:
+						{
+							keys[5] = 1;
+						} break;
+
+						case SDLK_e:
+						{
+							keys[6] = 1;
+						} break;
+
+						case SDLK_a:
+						{
+							keys[7] = 1;
+						} break;
+
+						case SDLK_s:
+						{
+							keys[8] = 1;
+						} break;
+
+						case SDLK_d:
+						{
+							keys[9] = 1;
+						} break;
+
+						case SDLK_z:
+						{
+							keys[0xA] = 1;
+						} break;
+
+						case SDLK_c:
+						{
+							keys[0xB] = 1;
+						} break;
+
+						case SDLK_4:
+						{
+							keys[0xC] = 1;
+						} break;
+
+						case SDLK_r:
+						{
+							keys[0xD] = 1;
+						} break;
+
+						case SDLK_f:
+						{
+							keys[0xE] = 1;
+						} break;
+
+						case SDLK_v:
+						{
+							keys[0xF] = 1;
+						} break;
+					}
+				} break;
+
+				case SDL_KEYUP:
+				{
+					switch (event.key.keysym.sym)
+					{
+						case SDLK_x:
+						{
+							keys[0] = 0;
+						} break;
+
+						case SDLK_1:
+						{
+							keys[1] = 0;
+						} break;
+
+						case SDLK_2:
+						{
+							keys[2] = 0;
+						} break;
+
+						case SDLK_3:
+						{
+							keys[3] = 0;
+						} break;
+
+						case SDLK_q:
+						{
+							keys[4] = 0;
+						} break;
+
+						case SDLK_w:
+						{
+							keys[5] = 0;
+						} break;
+
+						case SDLK_e:
+						{
+							keys[6] = 0;
+						} break;
+
+						case SDLK_a:
+						{
+							keys[7] = 0;
+						} break;
+
+						case SDLK_s:
+						{
+							keys[8] = 0;
+						} break;
+
+						case SDLK_d:
+						{
+							keys[9] = 0;
+						} break;
+
+						case SDLK_z:
+						{
+							keys[0xA] = 0;
+						} break;
+
+						case SDLK_c:
+						{
+							keys[0xB] = 0;
+						} break;
+
+						case SDLK_4:
+						{
+							keys[0xC] = 0;
+						} break;
+
+						case SDLK_r:
+						{
+							keys[0xD] = 0;
+						} break;
+
+						case SDLK_f:
+						{
+							keys[0xE] = 0;
+						} break;
+
+						case SDLK_v:
+						{
+							keys[0xF] = 0;
+						} break;
+					}
+				} break;
+			}
+		}
+
+		return quit;
+	}
+
+private:
+	SDL_Window* window{};
+	SDL_Renderer* renderer{};
+	SDL_Texture* texture{};
+};
+
+
 
 class Chip8 {
 
 public:
 
+    void loadRom(char const* fileName);
+    uint8_t keypad[KEY_COUNT]{};
+    uint32_t video[VIDEO_WIDTH][VIDEO_HEIGHT]{};
+    Chip8();
+    void Cycle();
+    
+private:
+
+    void Table0();
+    void Table8();
+    void TableE();
+    void TableF();
     uint8_t registers[16]{};
     uint8_t memory[4096]{};
     uint16_t index{};
@@ -55,44 +306,50 @@ public:
     uint8_t sp{};
     uint8_t delayTimer{};
     uint8_t soundTimer{};
-    uint8_t keypad[16]{};
-    uint32_t video[32][64]{};
     uint16_t opcode;
+    void OP_NULL();
+    void OP_00E0();
+    void OP_00EE();
+    void OP_1nnn();
+    void OP_2nnn();
+    void OP_3xkk();
+    void OP_4xkk();
+    void OP_5xy0();
+    void OP_6xkk();
+    void OP_7xkk();
+    void OP_8xy0();
+    void OP_8xy1();
+    void OP_8xy2();
+    void OP_8xy3();
+    void OP_8xy4();
+    void OP_8xy5();
+    void OP_8xy6();
+    void OP_8xy7();
+    void OP_8xyE();
+    void OP_9xy0();
+    void OP_Annn();
+    void OP_Bnnn();
+    void OP_Cxkk();
+    void OP_Dxyn();
+    void OP_Ex9E();
+    void OP_ExA1();
+    void OP_Fx07();
+    void OP_Fx0A();
+    void OP_Fx15();
+    void OP_Fx18();
+    void OP_Fx1E();
+    void OP_Fx29();
+    void OP_Fx33();
+    void OP_Fx55();
+    void OP_Fx65();
 
-private:
+    typedef void(Chip8::*Chip8Func)();
 
-    void loadRom(char const* fileName);
-    void Chip8::OP_00E0();
-    void Chip8::OP_00EE();
-    void Chip8::OP_1nnn();
-    void Chip8::OP_2nnn();
-    void Chip8::OP_3xkk();
-    void Chip8::OP_4xkk();
-    void Chip8::OP_5xy0();
-    void Chip8::OP_6xkk();
-    void Chip8::OP_7xkk();
-    void Chip8::OP_8xy0();
-    void Chip8::OP_8xy1();
-    void Chip8::OP_8xy2();
-    void Chip8::OP_8xy3();
-    void Chip8::OP_8xy4();
-    void Chip8::OP_8xy5();
-    void Chip8::OP_8xy6();
-    void Chip8::OP_8xy7();
-    void Chip8::OP_8xyE();
-    void Chip8::OP_9xy0();
-    void Chip8::OP_Annn();
-    void Chip8::OP_Bnnn();
-    void Chip8::OP_Cxkk();
-    void Chip8::OP_Dxyn();
-    void Chip8::OP_Ex9E();
-    void Chip8::OP_ExA1();
-    void Chip8::OP_Fx07();
-    void Chip8::OP_Fx0A();
-    void Chip8::OP_Fx15();
-    void Chip8::OP_Fx18();
-    void Chip8::OP_Fx1E();
-    void Chip8::OP_Fx29();
+    Chip8Func table[0xF + 1];
+    Chip8Func table0[0xE + 1];
+    Chip8Func table8[0xE + 1];
+    Chip8Func tableE[0xE + 1];
+    Chip8Func tableF[0x65 + 1];
 
 };
 
@@ -131,7 +388,113 @@ Chip8::Chip8(){
 
     }
 
+    table[0x0] = &Chip8::Table0;
+    table[0x1] = &Chip8::OP_1nnn;
+    table[0x2] = &Chip8::OP_2nnn;
+    table[0x3] = &Chip8::OP_3xkk;
+    table[0x4] = &Chip8::OP_4xkk;
+    table[0x5] = &Chip8::OP_5xy0;
+    table[0x6] = &Chip8::OP_6xkk;
+    table[0x7] = &Chip8::OP_7xkk;
+    table[0x8] = &Chip8::Table8;
+    table[0x9] = &Chip8::OP_9xy0;
+    table[0xA] = &Chip8::OP_Annn;
+    table[0xB] = &Chip8::OP_Bnnn;
+    table[0xC] = &Chip8::OP_Cxkk;
+    table[0xD] = &Chip8::OP_Dxyn;
+    table[0xE] = &Chip8::TableE;
+    table[0xF] = &Chip8::TableF;
+
+    for (size_t i = 0; i <= 0xE; ++i){
+
+        table0[i] = &Chip8::OP_NULL;
+        table8[i] = &Chip8::OP_NULL;
+        tableE[i] = &Chip8::OP_NULL;
+
+    }
+
+    table0[0x0] = &Chip8::OP_00E0;
+    table0[0x1] = &Chip8::OP_00EE;
+
+    table8[0x0] = &Chip8::OP_8xy0;
+    table8[0x1] = &Chip8::OP_8xy1;
+    table8[0x2] = &Chip8::OP_8xy2;
+    table8[0x3] = &Chip8::OP_8xy3;
+    table8[0x4] = &Chip8::OP_8xy4;
+    table8[0x5] = &Chip8::OP_8xy5;
+    table8[0x6] = &Chip8::OP_8xy6;
+    table8[0x7] = &Chip8::OP_8xy7;
+    table8[0xE] = &Chip8::OP_8xyE;
+
+    tableE[0x1] = &Chip8::OP_ExA1;
+    tableE[0xE] = &Chip8::OP_Ex9E;
+
+    for (size_t i = 0; i <= 0x65; ++i){
+
+        tableF[i] = &Chip8::OP_NULL;
+
+    }
+
+    tableF[0x07] = &Chip8::OP_Fx07;
+    tableF[0x0A] = &Chip8::OP_Fx0A;
+    tableF[0x15] = &Chip8::OP_Fx15;
+    tableF[0x18] = &Chip8::OP_Fx18;
+    tableF[0x1E] = &Chip8::OP_Fx1E;
+    tableF[0x29] = &Chip8::OP_Fx29;
+    tableF[0x33] = &Chip8::OP_Fx33;
+    tableF[0x55] = &Chip8::OP_Fx55;
+    tableF[0x65] = &Chip8::OP_Fx65;
+
 }
+
+void Chip8::Table0(){
+
+    ((*this).*(table0[opcode & 0x000Fu]))();
+
+}
+
+void Chip8::Table8(){
+
+    ((*this).*(table8[opcode & 0x000Fu]))();
+
+}
+
+void Chip8::TableE(){
+
+    ((*this).*(table8[opcode & 0x000Fu]))();
+
+}
+
+void Chip8::TableF(){
+
+    ((*this).*(table8[opcode & 0x00FFu]))();
+
+}
+
+void Chip8::Cycle(){
+
+    opcode = (memory[pc] << 8) | memory[pc + 1];
+
+    pc = pc + 2;
+
+    ((*this).*(table[(opcode & 0xF000u) >> 12]))();
+
+    if (delayTimer > 0){
+
+        --delayTimer;
+
+    }
+
+    if (soundTimer > 0){
+
+        --soundTimer;
+
+    }
+
+}
+
+
+void Chip8::OP_NULL(){}
 
 void Chip8::OP_00E0(){
 
@@ -593,13 +956,47 @@ void Chip8::OP_Fx1E(){
 
 }
 
-void Chip8::OP_Fx29(){ //unfinished
+void Chip8::OP_Fx29(){
 
-    
+    int x = (opcode & 0x0F00u) >> 8;
+
+    index = FONTSET_START_ADDRESS + (registers[x] * 5);
 
 }
 
+void Chip8::OP_Fx33(){
+    
+    int x = (opcode & 0x0F00u) >> 8;
 
+    memory[index] = (registers[x] / 100) % 10;
+    memory[index + 1] = (registers[x] / 10) % 10;
+    memory[index + 2] = registers[x] % 10;
+
+}
+
+void Chip8::OP_Fx55(){
+
+    int x = (opcode & 0x0F00u) >> 8;
+
+    for (int i = 0; i <= x; ++i){
+
+        memory[index + i] = registers[i];
+
+    }
+
+}
+
+void Chip8::OP_Fx65(){
+
+    int x = (opcode & 0x0F00u) >> 8;
+
+    for (int i = 0; i <= x; ++i){
+
+        registers[i] = memory[index + i];
+
+    }
+
+}
 
 
 
